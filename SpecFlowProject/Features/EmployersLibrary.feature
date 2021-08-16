cd90664663f95071010b1b1caf02a6dbd8c73751
Vlasov Alexei <alb883145@gmail.com>
2021-08-16 16:09:29 +0300

    Create SpecFlow project and added first test GetStuffOfficeEmployers_WhenAllPersonInOffice

---
+++ SpecFlowProject/Features/EmployersLibrary.feature
@@ -0,0 +1,17 @@
+﻿Feature: EmployersLibrary
+
+Background: Set employees in company
+	Given I create company with name 'FLS'
+	And I add persons to the company with name 'FLS'
+		| LastName | FirstName |
+		| Vlasov   | Alexei    |
+		| Vlasov   | Andrei    |
+		| Ivlev    | Andrei    |
+		| Filipov  | Vladimir  |
+
+		
+Scenario: GetStuffOfficeEmployers_WhenAllPersonInOffice
+	When I Get List Of All Office Employers as new Director of company 'FLS' and put it in scenario context 'actualResult'
+	When I Get List Of All Office Employers as new Director of company 'FLS' and put it in scenario context 'FLS.stuff'
+	Then I validate count of collection 'actualResult' is '4'
+	And I validate count of collection 'FLS.stuff' is '4'
---
+++ SpecFlowProject/Steps/EmployersLibrary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+using EmployersLibrary;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("I create company with name '(.*)'")]
+        public void ICreateCompanyWithName(string company)
+        {
+            _scenarioContext[company] = new Company();
+        }
+
+        [Given("I add persons to the company with name '(.*)'")]
+        public void IAddPersonsToTheCompanyWithName(string _company, Table table)
+        {
+            var company = (Company)_scenarioContext[_company];
+
+            foreach (var row in table.Rows)
+            {
+                var person = new Person() { LastName = row["LastName"], FirstName = row["FirstName"] };
+                company.AddPerson(person);
+            }
+        }
+
+        [When("I Get List Of All Office Employers as new Director of company '(.*)' and put it in scenario context '(.*)'")]
+        public void IGetListOfAllOfficeEmploytersAsNewDirectorOfCompanyAndPutItInScenarioContext(string _company, string _stuffCollection)
+        {
+            var company = (Company)_scenarioContext[_company];
+            var director = new Director(company);
+            var stuffCollection = director.GetListOfAllOfficeEmployers();
+            _scenarioContext[_stuffCollection] = stuffCollection;
+        }
+
+        [Then("I validate count of collection '(.*)' is '(.*)'")]
+        public void ThenIValidateCountOfCollection(string _collection, int expectedCount)
+        {
+            var collection = (List<Person>)_scenarioContext[_collection];
+            Assert.Equal(expectedCount, collection.Count);
+        }
+
+
+    }
+}
+
